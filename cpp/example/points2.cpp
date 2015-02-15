9f053fc5427eb45923bdc4908eaef2bdf4ab790d
Thomas Eding <thomaseding@gmail.com>
2015-02-14 17:38:49 -0800

    Fix build

--- cpp/example/points2.cpp
+++ cpp/example/points2.cpp
@@ -25,12 +25,12 @@ namespace lambda_opts
 {
 	template <typename Char>
 	struct RawParser<Char, Point> {
-		bool operator() (ArgsIter<Char> & iter, ArgsIter<Char> end, char * raw)
+		bool operator() (ParseState<Char> parseState, void * rawMemory)
 		{
 			Maybe<std::array<float, 3>> maybeVals;
-			if (Parse<Char, std::array<float, 3>>(iter, end, maybeVals)) {
+			if (Parse<Char, std::array<float, 3>>(parseState, maybeVals)) {
 				auto & vals = *maybeVals;
-				new (raw) Point(vals[0], vals[1], vals[2]);
+				new (rawMemory) Point(vals[0], vals[1], vals[2]);
 				return true;
 			}
 			return false;
