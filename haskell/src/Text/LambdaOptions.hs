10379c387665ecd1ae0fdd5e93614f71760f4447
Thomas Eding <thomaseding@gmail.com>
2015-05-16 01:11:45 -0700

    Escape for haddock

--- haskell/src/Text/LambdaOptions.hs
+++ haskell/src/Text/LambdaOptions.hs
@@ -322,27 +322,25 @@ mkParseFailed' beginIndex endIndex args
 --
 -- Example:
 --
--- @
--- import System.Environment
--- import Text.LambdaOptions
--- 
--- options :: Options IO ()
--- options = do
---     addOption (kw "--help") $ do
---         putStrLn "--user NAME [AGE]"
---     addOption (kw "--user") $ \name -> do
---         putStrLn $ "Name:" ++ name
---     addOption (kw "--user") $ \name age -> do
---         putStrLn $ "Name:" ++ name ++ " Age:" ++ show (age :: Int)
--- 
--- main :: IO ()
--- main = do
---     args <- getArgs
---     mError <- runOptions options args
---     case mError of
---         Just (ParseFailed msg _ _) -> putStrLn msg
---         Nothing -> return ()
--- @
+-- > import System.Environment
+-- > import Text.LambdaOptions
+-- > 
+-- > options :: Options IO ()
+-- > options = do
+-- >     addOption (kw "--help") $ do
+-- >         putStrLn "--user NAME [AGE]"
+-- >     addOption (kw "--user") $ \name -> do
+-- >         putStrLn $ "Name:" ++ name
+-- >     addOption (kw "--user") $ \name age -> do
+-- >         putStrLn $ "Name:" ++ name ++ " Age:" ++ show (age :: Int)
+-- > 
+-- > main :: IO ()
+-- > main = do
+-- >     args <- getArgs
+-- >     mError <- runOptions options args
+-- >     case mError of
+-- >         Just (ParseFailed msg _ _) -> putStrLn msg
+-- >         Nothing -> return ()
 runOptions :: (Monad m) => Options m a -> [String] -> m (Maybe OptionsError)
 runOptions action args = runOptions' args $ runStateT (unOptions $ action >> tryParseAll) $ OptionsState {
     stateOpaqueParsers = Map.empty,
